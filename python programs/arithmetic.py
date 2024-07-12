32842317f914e74c4e97d3f11fb224a45ec8d4f6
SAKSHI PATIL <127477154+Sakshipatil1611@users.noreply.github.com>
2024-07-12 09:07:19 +0530

    Update arithmetic.py

---
+++ python programs/arithmetic.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Addition
+sum = num1 + num2
+
+# Output the result
+print("The sum of", num1, "and", num2, "is:", sum)
